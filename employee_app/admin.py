a552054d789726d89c5d71ba9343542547085b4f
EMON51 <emanur11235@gmail.com>
2025-08-17 11:04:10 +0600

    Add Employee, Department, Achievement and AchievementEmployee models

--- employee_app/admin.py
+++ employee_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . models import Department, Achievement, Employee, AchievementEmployee
 
 # Register your models here.
+
+admin.site.register(Department)
+admin.site.register(Achievement)
+admin.site.register(Employee)
+admin.site.register(AchievementEmployee)
--- employee_app/models.py
+++ employee_app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class Department(models.Model):
+    #id is default attribute 
+    name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name
+
+class Achievement(models.Model):
+    #id is default attribute 
+    name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    #id is default attribute 
+    name = models.CharField(max_length = 100)
+    email = models.EmailField(unique = True)
+    phone = models.CharField(max_length = 15)
+    address = models.TextField(blank = True)
+    department = models.ForeignKey(Department, on_delete = models.CASCADE)
+    achievements = models.ManyToManyField(Achievement, through = "AchievementEmployee")
+    
+    def __str__(self):
+        return self.name
+
+
+class AchievementEmployee(models.Model):
+    achievement = models.ForeignKey(Achievement, on_delete = models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete = models.CASCADE)
+    achievement_date = models.DateField()
+
+
