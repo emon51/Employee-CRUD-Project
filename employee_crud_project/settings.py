82c2f513e332d87f826471c6dbbf3de1062b338c
EMON51 <emanur11235@gmail.com>
2025-08-17 11:21:52 +0600

    Add Templates

--- employee_crud_project/settings.py
+++ employee_crud_project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'employee_crud_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
