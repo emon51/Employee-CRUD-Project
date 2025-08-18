970c30b688d3d1691cacb0b69b2cd1d7e7f01bf5
EMON51 <emanur11235@gmail.com>
2025-08-18 13:55:33 +0600

    Add user registration features and implement corresponding forms and views

--- account_app/urls.py
+++ account_app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('account-view', views.account_view, name = 'account-view'), 
+     path("register/", views.register, name = "register"),
 ]
--- account_app/views.py
+++ account_app/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+
+
+from django.contrib.auth import login
+
+from django import forms 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 
-def account_view(request):
-    return HttpResponse('This is account view.')
+
+# Custom form that takes email and password from the user.
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["email", "password1", "password2"]
+
+
+# Registration
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = form.cleaned_data["email"]  
+            user.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = RegisterForm()
+    return render(request, "account_app/register.html", {"form": form})
+
+
+
 
